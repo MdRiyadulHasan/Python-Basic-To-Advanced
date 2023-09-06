a964ac1baec6d2bf7356e394e36ecfaeadcc1dcf
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-09-06 22:30:22 +0600

    Create 1. Subarray_sum_size_k.py

---
+++ Advanced_Python/1. Subarray_sum_size_k.py
@@ -0,0 +1,16 @@
+def max_subarray_sum(arr, k):
+    if k <= 0 or k > len(arr):
+        return "Invalid input: k is out of range"
+
+    windowSum = sum(arr[:k])
+    maxSum = sum(arr[:k])
+    for i in range(k, len(arr)):
+        windowSum = windowSum + arr[i] - arr[i-k]
+        maxSum = max(maxSum, windowSum)
+    return maxSum
+if __name__ == '__main__':
+    arr = [20, 61, 55, 41, 3, 29]
+    k = 3
+    result = max_subarray_sum(arr, k)
+    print("Maximum subarray sum of size", k, "is:", result)
+
