2dcd36e7ea7d8358d5f5bebe068b68aae62f7c27
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-10-07 11:08:36 +0600

    Create 4.decorators.py

---
+++ Decorators/4.decorators.py
@@ -0,0 +1,21 @@
+import time
+
+# Decorator function to measure execution time
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # Record the start time
+        result = func(*args, **kwargs)  # Call the original function
+        end_time = time.time()  # Record the end time
+        elapsed_time = end_time - start_time  # Calculate the elapsed time
+        print(f"{func.__name__} took {elapsed_time} seconds to execute.")
+        return elapsed_time
+    return wrapper
+
+# Apply the decorator to a function
+@timing_decorator
+def slow_function():
+    time.sleep(2)  # Simulate a time-consuming operation
+
+# Call the decorated function
+ans = slow_function()
+print(ans)
